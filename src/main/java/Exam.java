5481cf406519743ebabd61fc1962b060199ea76e
unknown <raje1reddy@gmail.com>
2019-08-06 17:53:02 +0100

    objects accessibility scope in Java

---
+++ src/main/java/Exam.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+import java.util.ArrayList;
+//code accesses and prints all the elements of array list
+public class Exam {
+    String name;
+
+    public void setName(String newName) {
+        name = newName;
+        System.out.println(name);
+    }
+}
+
